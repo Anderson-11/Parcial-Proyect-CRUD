c8c9f4e94bef8bb88973eef566dff7417953b875
Anderson11 <CQ0472032023@unab.edu.sv>
2024-08-27 22:24:53 -0600

    Creacion de clases para el manejo de los datos y enlazar la cadendena de conexion con la interfaz grafica.

---
+++ Proyecto-CRUD/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Proyecto_CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.btnVer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAñadir.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAñadir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAñadir.Location = new System.Drawing.Point(83, 162);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(134, 43);
+            this.btnAñadir.TabIndex = 5;
+            this.btnAñadir.Text = "Añadir Personal";
+            this.btnAñadir.UseVisualStyleBackColor = false;
+            // 
+            // btnVer
+            // 
+            this.btnVer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnVer.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnVer.Location = new System.Drawing.Point(283, 162);
+            this.btnVer.Name = "btnVer";
+            this.btnVer.Size = new System.Drawing.Size(134, 43);
+            this.btnVer.TabIndex = 4;
+            this.btnVer.Text = "Ver Listado";
+            this.btnVer.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Georgia", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(32, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 43);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "⁓Registro De Personal⁓";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(522, 296);
+            this.Controls.Add(this.btnAñadir);
+            this.Controls.Add(this.btnVer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAñadir;
+        private System.Windows.Forms.Button btnVer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Proyecto-CRUD/Mantenimiento.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Proyecto_CRUD
+{
+    partial class Mantenimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.text6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text3 = new System.Windows.Forms.TextBox();
+            this.text5 = new System.Windows.Forms.TextBox();
+            this.text4 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnviarDatos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnModificar.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnModificar.Location = new System.Drawing.Point(305, 408);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(180, 44);
+            this.btnModificar.TabIndex = 53;
+            this.btnModificar.Text = "Editar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(376, 305);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 18);
+            this.label9.TabIndex = 52;
+            this.label9.Text = "Pais";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label14.Location = new System.Drawing.Point(120, 402);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(65, 18);
+            this.label14.TabIndex = 51;
+            this.label14.Text = "Telefono";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(64, 424);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(171, 20);
+            this.textBox7.TabIndex = 50;
+            // 
+            // text6
+            // 
+            this.text6.Location = new System.Drawing.Point(305, 327);
+            this.text6.Name = "text6";
+            this.text6.Size = new System.Drawing.Size(171, 20);
+            this.text6.TabIndex = 49;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(329, 111);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(127, 18);
+            this.label8.TabIndex = 48;
+            this.label8.Text = "Nombre Contacto";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(84, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 18);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Titulo de Contacto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(367, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 18);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Ciudad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(120, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 18);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Postal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(84, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 18);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Nombre Compañia";
+            // 
+            // text3
+            // 
+            this.text3.Location = new System.Drawing.Point(64, 230);
+            this.text3.Name = "text3";
+            this.text3.Size = new System.Drawing.Size(171, 20);
+            this.text3.TabIndex = 43;
+            // 
+            // text5
+            // 
+            this.text5.Location = new System.Drawing.Point(64, 327);
+            this.text5.Name = "text5";
+            this.text5.Size = new System.Drawing.Size(171, 20);
+            this.text5.TabIndex = 42;
+            // 
+            // text4
+            // 
+            this.text4.Location = new System.Drawing.Point(305, 230);
+            this.text4.Name = "text4";
+            this.text4.Size = new System.Drawing.Size(171, 20);
+            this.text4.TabIndex = 41;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(305, 138);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(171, 20);
+            this.text2.TabIndex = 40;
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(64, 138);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(171, 20);
+            this.text1.TabIndex = 39;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Georgia", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(133, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 34);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "⁓Añadir Personal⁓";
+            // 
+            // btnEnviarDatos
+            // 
+            this.btnEnviarDatos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnEnviarDatos.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviarDatos.ForeColor = System.Drawing.Color.White;
+            this.btnEnviarDatos.Location = new System.Drawing.Point(305, 408);
+            this.btnEnviarDatos.Name = "btnEnviarDatos";
+            this.btnEnviarDatos.Size = new System.Drawing.Size(180, 44);
+            this.btnEnviarDatos.TabIndex = 54;
+            this.btnEnviarDatos.Text = "Aceptar";
+            this.btnEnviarDatos.UseVisualStyleBackColor = false;
+            // 
+            // Mantenimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(554, 500);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.text6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.text3);
+            this.Controls.Add(this.text5);
+            this.Controls.Add(this.text4);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnviarDatos);
+            this.Name = "Mantenimiento";
+            this.Text = "Mantenimiento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox text6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text3;
+        private System.Windows.Forms.TextBox text5;
+        private System.Windows.Forms.TextBox text4;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnviarDatos;
+    }
+}
