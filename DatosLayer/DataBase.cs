bc8a3efb305de237f75d6972f52e178ad55fb22c
Anderson11 <CQ0472032023@unab.edu.sv>
2024-08-27 22:34:54 -0600

    Estilos de la interfaz grafica y la crecion de las metodos CRUD

--- DatosLayer/DataBase.cs
+++ DatosLayer/DataBase.cs
@@ -10,6 +10,30 @@ namespace DatosLayer
 {
     public class DataBase
     {
-        
+        public static int ConnectionTimeout { get; set; }
+        public static string ApplicationName { get; set; }
+        public static string ConnectionString
+        {
+            get
+            {
+                string CadenaConexion = ConfigurationManager.ConnectionStrings["NWConnection"].ConnectionString;
+
+                SqlConnectionStringBuilder conexionBuilder = new SqlConnectionStringBuilder(CadenaConexion);
+
+                conexionBuilder.ApplicationName = ApplicationName ?? conexionBuilder.ApplicationName;
+
+                conexionBuilder.ConnectTimeout = (ConnectionTimeout > 0) ? ConnectionTimeout : conexionBuilder.ConnectTimeout;
+                return conexionBuilder.ToString();
+            }
+        }
+
+        public static SqlConnection GetSqlConnection()
+        {
+
+            SqlConnection conexion = new SqlConnection(ConnectionString);
+            conexion.Open();
+            return conexion;
+
+        }
     }
 }
